4b5a3d3bf237f1a0324d0fa8e562ec0b801dfdb9
chint <chint@digi-texx.vn>
2019-03-25 15:31:41 +0700

    up

---
+++ CTComputer/src/main/java/com/admin/service/OrderDetailService.java
@@ -0,0 +1,79 @@
+package com.admin.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.admin.dao.OrderDetailDAO;
+import com.model.OrderDetail;
+
+@Service("orderDetailService")
+@Transactional
+public class OrderDetailService {
+
+	@Autowired
+	OrderDetailDAO orderDetailDAO;
+
+	/**
+	 * Sửa Hóa Đơn
+	 * 
+	 * @param order
+	 */
+	public void updateOrderDetail(OrderDetail order) {
+		orderDetailDAO.updateOrderDetail(order);
+	}
+
+	/**
+	 * Xóa hóa đơn
+	 * 
+	 * @param order
+	 */
+	public void deleteOrderDetail(OrderDetail order) {
+		orderDetailDAO.deleteOrderDetail(order);
+	}
+
+	/**
+	 * Lấy id hóa đơn
+	 * 
+	 * @param id
+	 * @return
+	 */
+	public OrderDetail getIDOrderDetail(Integer id) {
+		return orderDetailDAO.getIDOrderDetail(id);
+	}
+
+	/**
+	 * Lấy danh sách tất cả hóa đơn
+	 * 
+	 * @return
+	 */
+	public List<OrderDetail> getAllOrderDetail() {
+		return orderDetailDAO.getAllOrderDetail();
+	}
+
+	/**
+	 * Tìm kiếm hóa đơn
+	 * 
+	 * @param search
+	 * @return
+	 */
+	public List<OrderDetail> searchOrderDetail(String search) {
+		return orderDetailDAO.searchOrderDetail(search);
+	}
+
+	/**
+	 * Phân trang
+	 * 
+	 * @param page
+	 * @return
+	 */
+	public List<OrderDetail> loadOrderDetailPage(String page) {
+		return orderDetailDAO.loadOrderDetailPage(page);
+	}
+
+	public int getRowOrderDetail() {
+		return orderDetailDAO.getRowOrderDetail();
+	}
+}
