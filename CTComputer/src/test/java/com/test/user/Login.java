1d6531fae4002e528bd7e729136a1d63ede37d69
NGUYỄN TRUNG CHÍ <trungchi92@gmail.com>
2019-04-15 15:20:21 +0700

    unit test

--- CTComputer/src/test/java/com/test/user/Login.java
+++ CTComputer/src/test/java/com/test/user/Login.java
@@ -1,23 +1,30 @@
 package com.test.user;
 
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import com.admin.controller.BasicController;
-import com.admin.service.CustomerService;
 import com.model.Customer;
 
-public class Login extends BasicController {
+public class Login {
 	@Autowired
-	CustomerService customerService;
+	SessionFactory sessionFactory;
 	
 	public boolean checkValidCustomer(String username, String password){
-		Customer customer = new Customer();
-		customer.setId(username);
-		Session session = sessionFactory.getCurrentSession();
-		session.refresh(customer);
-		if(customer.getId().equals(username))
-			return true;
+		try {
+			Customer customer = new Customer();
+			customer.setId(username);
+			Session session = sessionFactory.getCurrentSession();
+			session.refresh(customer);
+			if(customer.getId().equals(username))
+				return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		return false;
+	}
+	public static void main(String[] args) {
+		Login login = new Login();
+		login.checkValidCustomer("trungchi", "123456");
 	}
 }
